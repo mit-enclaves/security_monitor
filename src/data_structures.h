a96b268b167cfd0109c5718bca4147face4afe6d
Jules Drean <drean@mit.edu>
2019-03-19 15:57:59 -0400

    Mailboxe struct and enum fix

--- src/data_structures.h
+++ src/data_structures.h
@@ -1,6 +1,8 @@
 #ifndef SECURITY_MONITOR_DATA_STRUCTURES_H
 #define SECURITY_MONITOR_DATA_STRUCTURES_H
-#include <cstdint>
+
+#include <stdint.h>
+#include "constants.h"
 
 typedef uint64_t phys_ptr_t;
 typedef uint64_t hash_t[8];
@@ -15,19 +17,17 @@ typedef struct {
 	uint64_t pad[7];
 }atomic_flag_t;
 
-static inline bool atomic_flag_test_and_set(atomic_flag_t f) {
-	return __sync_lock_test_and_set(&f.flag, true);
-}
-
 // MAILBOX
+typedef phys_ptr_t enclave_id_t;
 
 typedef struct{
-
+	enclave_id_t expected_sender;
+	bool has_message;
+	uint8_t message[MAILBOX_SIZE];
 }mailbox_t;
 
 // ENCLAVE
 
-typedef phys_ptr_t enclave_id_t;
 
 typedef struct {
 	bool initialized;
@@ -62,10 +62,10 @@ typedef uint64_t metadata_page_map_entry;
 typedef metadata_page_map_entry metadata_page_map_t[NUM_PAGES_PER_REGION];
 
 typedef enum { // TODO must fit into 12 bits
-	invalid = 0;
-	free    = 1;
-	enclave = 2;
-	thread  = 3;
+	invalid = 0,
+	free    = 1,
+	enclave = 2,
+	thread  = 3,
 }metadata_page_t;
 
 // CORE
@@ -80,14 +80,18 @@ typedef struct {
 // DRAM REGION
 
 typedef enum {
-  dram_region_invalid = 0,
-  dram_region_free = 1,
-  dram_region_blocked = 2,
-  dram_region_locked = 3,
-  dram_region_owned = 4,
+	dram_region_invalid = 0,
+	dram_region_free = 1,
+	dram_region_blocked = 2,
+	dram_region_locked = 3,
+	dram_region_owned = 4,
 } dram_region_state_t;
 
-typedef struct {
+typedef enum {
+	untrusted_region = 0,
+	enclave_region = 1,
+	metadata_region = 2,
+	security_monitor_region = 3,
 }dram_region_type_t;
 
 typedef struct {
@@ -96,5 +100,13 @@ typedef struct {
 	dram_region_state_t state;
 	atomic_flag_t lock;
 }region_t;
+
+// SECURITY MONITOR
+
+typedef struct {
+	core_t cores[NUM_CORES];
+	region_t regions[NUM_REGIONS];
+
+}security_monitor_t
 
 #endif // SECURITY_MONITOR_DATA_STRUCTURES_H
