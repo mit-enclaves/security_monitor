323a80ac51eea09cd1335133ea61e804e9a07117
Jules Drean <drean@mit.edu>
2019-03-19 16:06:03 -0400

    Constants header file

---
+++ src/constants.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define SIZE_DRAM (0x80000000)
+#define SIZE_PAGE (0x1000)
+#define NUM_CORES 2
+#define NUM_REGIONS 64
+#define NUM_PAGES_PER_REGION (0x2000)
+
+#define MAILBOX_SIZE (128)
+
+#endif // CONSTANTS_H
