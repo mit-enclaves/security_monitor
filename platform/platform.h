ac47962cdf9b881b6b2b9e7427fe52c73a008e5e
Ilia Lebedev <ilebedev@mit.edu>
2020-01-14 16:14:19 -0500

    cleaned up platform stubs

--- platform/platform.h
+++ platform/platform.h
@@ -16,10 +16,10 @@
 
 // Core state
 // ----------
-void platform_save_core( platform_core_state_t * core_state );
-void platform_load_core( const platform_core_state_t * core_state );
-void platform_clean_core(void);
-void platform_purge_core (void);
+//void platform_save_core( platform_core_state_t * core_state );
+//void platform_load_core( const platform_core_state_t * core_state );
+void platform_clean_core(void); // arch state only
+void platform_purge_core (void); // uarch state only
 
 // Platform control and utilities
 // ------------------------------
@@ -32,9 +32,16 @@ void platform_purge_core (void);
   asm volatile ("ld " #reg ", %0" :: "rK"(val)); })
 
 
-void platform_panic (uint64_t error_code) __attribute__((noreturn));
+void platform_init ();
+void platform_core_init ();
+
+void platform_interrupt_other_cores ();
+void platform_wait_for_interrupt ();
+
 void platform_delegate_to_untrusted ( uint64_t virtual_pc, uint64_t  ) __attribute__((noreturn));
 void platform_jump_to_untrusted ( region_map_t * region_map, uint64_t virtual_pc, uint64_t virtual_sp ) __attribute__((noreturn));
 void platform_jump_to_enclave ( enclave_id_t enclave_id, uint64_t virtual_pc, uint64_t ) __attribute__((noreturn));
+
+void platform_panic (uint64_t error_code) __attribute__((noreturn));
 
 #endif // SM_PLATFORM_H
