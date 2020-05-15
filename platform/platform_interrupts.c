f092c64a74cb983db6d1902428e930af98be2f7f
Jules Drean <drean@mit.edu>
2020-05-15 07:39:08 -0400

    Disactivate interrupt inside enclave

--- platform/platform_interrupts.c
+++ platform/platform_interrupts.c
@@ -1,7 +1,7 @@
 #include <sm.h>
 
 void platform_interrupts_enter_enclave(thread_metadata_t *thread_metadata) {
-  uint64_t mie_tmp = MIP_MTIP;
+  uint64_t mie_tmp = 0; //MIP_MTIP;
   thread_metadata->platform_csr.mie = swap_csr(mie, mie_tmp);
 }
 
