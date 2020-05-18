c3b8514818663760a2258ccd928b4b7d376fb459
Jules Drean <drean@mit.edu>
2020-05-18 16:58:41 -0400

    Make memncmp data oblivious

--- src/clib/memncmp.c
+++ src/clib/memncmp.c
@@ -2,10 +2,12 @@
 
 int memncmp(const void* s1, const void* s2,size_t n)
 {
-  bool res = false;
   const unsigned char *p1 = s1, *p2 = s2;
+  int pdiff;
+  int res = 0;
   while(n--) {
-    res = res || ((*p1 - *p2) != 0);
+    pdiff = *p1 - *p2;
+    res = res | pdiff;
     p1++,p2++;
   }
   return res;
