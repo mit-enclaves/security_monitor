919fd133b8c70a014be29cac74891f517b7b5a62
Jules Drean <drean@mit.edu>
2022-11-15 12:51:41 -0500

    Added lock on untrusted console code

--- untrusted_infra/sbi/console.c
+++ untrusted_infra/sbi/console.c
@@ -5,11 +5,16 @@
 #include <string.h>
 #include <stdarg.h>
 #include <stdbool.h>
+#include <platform_lock.h>
+
+static volatile platform_lock_t putstring_lock;
 
 void putstring(const char* s)
 {
+  while(!platform_lock_acquire(&putstring_lock)) {};
   while (*s)
     console_putchar(*s++);
+  platform_lock_release(&putstring_lock);
 }
 
 int vsnprintf(char* out, size_t n, const char* s, va_list vl)
