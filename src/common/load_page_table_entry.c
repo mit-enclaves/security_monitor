e7ac979ac79ffce4ce9204ee130fbd391cc34dcd
drean <drean@mit.edu>
2023-01-31 20:10:24 +0000

    Solve concurency issues for enclave page table load function calls

--- src/common/load_page_table_entry.c
+++ src/common/load_page_table_entry.c
@@ -20,33 +20,39 @@ api_result_t load_page_table_entry (enclave_id_t enclave_id, uintptr_t phys_addr
   else {
     uintptr_t pte_base = (enclave_metadata->platform_csr.eptbr & SATP_PPN_MASK) << PAGE_SHIFT;
     int pte_region_id = addr_to_region_id(pte_base);
-    if(!(locked_regions->flags[pte_region_id]) || (region_owner_lock_free(pte_region_id) != enclave_id)){
-       return MONITOR_INVALID_STATE;
+    if( !(((locked_regions->flags[pte_region_id]) && (region_owner_lock_free(pte_region_id) == enclave_id)) 
+          ||
+          ((!locked_regions->flags[pte_region_id]) && (region_owner(pte_region_id) == enclave_id)))
+      ){
+      return MONITOR_INVALID_STATE;
     }
 
     for(int i = 2; i >= (int) level; i--) {
-       uint64_t * pte_addr = (uint64_t *) ((uint64_t) pte_base +
+      uint64_t * pte_addr = (uint64_t *) ((uint64_t) pte_base +
           (((virtual_addr >> (PAGE_SHIFT + (PN_OFFSET * i))) & PN_MASK) * PTE_SIZE));
 
-       if(i != level) {
-          uint64_t pte_acl = (*pte_addr) & ACL_MASK;
-          if(((pte_acl & PTE_V) == 0) ||
-                (((pte_acl & PTE_R) == 0) && ((pte_acl & PTE_W) == PTE_W)) ||
-                ((pte_acl & PTE_R) == PTE_R) || ((pte_acl & PTE_X) == PTE_X)    ) {
-             return MONITOR_INVALID_STATE;
-          }
+      if(i != level) {
+        uint64_t pte_acl = (*pte_addr) & ACL_MASK;
+        if(((pte_acl & PTE_V) == 0) ||
+            (((pte_acl & PTE_R) == 0) && ((pte_acl & PTE_W) == PTE_W)) ||
+            ((pte_acl & PTE_R) == PTE_R) || ((pte_acl & PTE_X) == PTE_X)    ) {
+          return MONITOR_INVALID_STATE;
+        }
 
-          pte_base = (((*pte_addr) & PPNs_MASK) >> PAGE_ENTRY_ACL_OFFSET)  << PAGE_SHIFT;
-          pte_region_id = addr_to_region_id(pte_base);
-          if(!(locked_regions->flags[pte_region_id]) || (region_owner_lock_free(pte_region_id) != enclave_id)) {
-             return MONITOR_INVALID_STATE;
-          }
-       }
-       else {
-          *pte_addr = 0 |
-             ((((phys_addr >> PAGE_SHIFT) << PAGE_ENTRY_ACL_OFFSET) & (PPNs_MASK)) |
-             (acl & ACL_MASK));
-       }
+        pte_base = (((*pte_addr) & PPNs_MASK) >> PAGE_ENTRY_ACL_OFFSET)  << PAGE_SHIFT;
+        pte_region_id = addr_to_region_id(pte_base);
+        if( !(((locked_regions->flags[pte_region_id]) && (region_owner_lock_free(pte_region_id) == enclave_id)) 
+              ||
+              ((!locked_regions->flags[pte_region_id]) && (region_owner(pte_region_id) == enclave_id)))
+          ){
+          return MONITOR_INVALID_STATE;
+        }
+      }
+      else {
+        *pte_addr = 0 |
+          ((((phys_addr >> PAGE_SHIFT) << PAGE_ENTRY_ACL_OFFSET) & (PPNs_MASK)) |
+           (acl & ACL_MASK));
+      }
     }
   }
 
