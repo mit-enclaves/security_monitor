eda7ebf98987d28f52901544f6ac4a898ef7a99f
drean <drean@mit.edu>
2023-02-05 23:12:27 +0000

    Debugging help for test enclave

--- test/enclave/enclave_code.c
+++ test/enclave/enclave_code.c
@@ -2,7 +2,14 @@
 
 #define SHARED_MEM_REG (0x8a000000)
 
+#if (DEBUG_ENCLAVE == 1)
+#include "../sbi/console.h"
+#endif
+
 void enclave_entry() {
+#if (DEBUG_ENCLAVE == 1)
+  printm("Hi from inside!\n");
+#endif
   int a = 0;
   a += 90;
   sm_exit_enclave();
