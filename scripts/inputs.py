3c05e459b4ba0201db41f281766e69beccd9e07a
drean <drean@mit.edu>
2023-02-06 14:00:39 +0000

    1MB input data for experiements

--- scripts/inputs.py
+++ scripts/inputs.py
@@ -4,8 +4,9 @@ import sys
 import random
 import string
 
-len_a = 12
+len_a = 256 * 12
 len_elements = [40, 40, 40, 40, 40, 40, 40, 576, 576, 576, 576, 1500]
+len_b = len(len_elements)
 
 def randStr(N, chars = string.hexdigits):
     return ''.join(random.choice(chars) for _ in range(N))
@@ -19,13 +20,13 @@ with open(file_name, 'w') as f:
 
     f.write("int len_elements[] = {")
     for i in range(len_a):
-        f.write(str(len_elements[i]))
+        f.write(str(len_elements[i%len_b]))
         f.write(", ")
     f.write("};\n")
 
     f.write("char *a[] = {")
     for i in range(len_a):
         f.write("\"")
-        f.write(randStr(len_elements[i]))
+        f.write(randStr(len_elements[i%len_b]))
         f.write("\", ")
     f.write("};\n")
